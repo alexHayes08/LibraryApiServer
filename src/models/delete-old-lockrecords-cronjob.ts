2c32ad225a8e6ff62d0ffaad503e735704e0a33c
Alexander <alex.c.hayes08@gmail.com>
2018-08-03 17:12:36 -0500

    Latest

--- src/models/delete-old-lockrecords-cronjob.ts
+++ src/models/delete-old-lockrecords-cronjob.ts
@@ -5,6 +5,7 @@ import { TYPES } from '../dependency-registrar';
 import { LockService } from '../services/lock-service';
 import { PaginationResults, Paginate } from './paginate';
 import { LockRecord } from './lock';
+import { resolve } from 'url';
 
 function executePromisesSequentially<T>(promises: Array<Promise<T>>): Promise<T> {
     return promises.reduce((p, f) => p.then(f), Promise.resolve());
@@ -14,39 +15,26 @@ export const deleteOldLockRecordsCronJob: CronJobData = {
     id: 'fed584f6-9d30-4dc8-9983-73cf4c965749',
     name: 'Delete Old Lock Records',
     every: '',
-    run: async function (container: Container): Promise<boolean> {
-        const lockRecordService = container
-            .get<LockService>(TYPES.LockService);
+    run: function (container: Container): Promise<boolean> {
+        return new Promise(async function (resolve, reject) {
+            const lockRecordService = container
+                .get<LockService>(TYPES.LockService);
 
-        let nextResults: Paginate<LockRecord>|undefined = undefined;
+            let nextResults: Paginate<LockRecord>|undefined = undefined;
 
-        async function* generator() {
-            lockRecordService.paginate({
-                limit: 10
-            }).then(results => {
+            do {
+                const results = await lockRecordService.paginate({
+                    limit: 10
+                });
                 nextResults = results.next;
-                yield results.results;
-            });
+            } while (nextResults !== undefined);
 
-            while (nextResults !== undefined) {
-                const results = await lockRecordService.paginate(nextResults);
-                nextResults = results.next;
-                yield results.results;
+            const iterator = generator();
+            for await (const oldLocks of iterator) {
+                console.log(oldLocks);
             }
-        }
 
-        do {
-            const results = lockRecordService.paginate({
-                limit: 10
-            });
-        } while (nextResults !== undefined);
-
-        const iterator = generator();
-        const lockRecords = Array.from(iterator);
-        for await (const oldLocks of iterator) {
-            console.log(oldLocks);
-        }
-
-        return Promise.resolve(true);
+            resolve(true);
+        });
     }
 };
